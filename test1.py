0040417243e6fdcc4b61bc1494a422fbca727dc7
Evgeny Boger <boger@contactless.ru>
2014-05-25 18:41:49 +0400

    add timeout

--- test1.py
+++ test1.py
@@ -4,6 +4,26 @@ import time
 from registers import *
 import rfm69
 
+from functools import wraps
+import errno
+import os
+import signal
+
+class TimeoutError(Exception):
+    pass
+
+class Timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
 
 if len(sys.argv) > 2:
 	spi_minor = int(sys.argv[1])
@@ -13,20 +33,20 @@ else:
 	spi_minor = 5
 	irq_gpio = 36
 
-radio = rfm69.RFM69(spi_minor=spi_minor,irq_gpio=irq_gpio)
 
-radio.start()
+with Timeout(seconds=10):
+	radio = rfm69.RFM69(spi_minor=spi_minor,irq_gpio=irq_gpio)
 
+	radio.start()
+	#~ radio.setHighPower(True)
+	radio.setHighPower(False)
+	radio.receiveBegin()
 
-#~ radio.setHighPower(True)
-radio.setHighPower(False)
-radio.receiveBegin()
-
-sys.exit(0)
-#~
-#~ while 1:
-	#~ data = radio.receiveDone()
+	sys.exit(0)
 	#~
+	#~ while 1:
+		#~ data = radio.receiveDone()
+		#~
 
 while 1:
 	raw_input(">>")
